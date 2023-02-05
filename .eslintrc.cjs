4291cdf1ac70f3e474073b545572afa3e89b77e3
D:/Program Files/DevTools/Git/jxk <1286783182@qq.com>
2023-02-05 18:46:57 +0800

    添加提交校验

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,26 @@
 module.exports = {
-  extends: [
-    'semistandard',
-    'standard'
-  ]
-}
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended'
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always']
+	}
+};
