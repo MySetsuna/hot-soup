5611be8592618dc7940cd4288fc9e5d06e64ec4b
jack <1286783182@qq.com>
2023-02-07 11:25:50 +0800

    build: set the base

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,7 @@ function App () {
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<p>build a blog site with friend</p>
 		</div>
 	);
 }
