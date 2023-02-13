f6521cbb36675c6406883ba6639f3f7d5dc6aeb3
jack <1286783182@qq.com>
2023-02-13 15:29:17 +0800

    build: true way on gh-page stackoverflow 70395443

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -8,21 +8,12 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import UserInfoProvider from './contexts/UseInfoProvider';
 const BASE_URL = import.meta.env.BASE_URL;
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<HashRouter>
+		<BrowserRouter basename={BASE_URL}>
 			<UserInfoProvider>
-				<Routes>
-					<Route
-						// loader={(params) => {
-						// 	console.log(params);
-						// 	redirect(BASE_URL);
-						// }}
-						path={`${BASE_URL}*`}
-						element={<App />}
-					/>
-				</Routes>
+				<App />
 			</UserInfoProvider>
-		</HashRouter>
+		</BrowserRouter>
 	</React.StrictMode>
 );
