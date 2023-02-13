90f88a0ac671282a242a9c32cf34fd8920103064
jack <1286783182@qq.com>
2023-02-13 14:48:53 +0800

    build: test

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,14 @@
 # 发生错误时终止
 set -e
 
+#移除dist
+yarn run rimraf dist
+
 # 先进行测试
-yarn run test
+yarn run coverage
 
 # 构建
-yarn run build:gh
+yarn run build
 
 # 进入构建文件夹
 cd dist
@@ -26,7 +29,7 @@ git commit -m 'deploy'
 # 如果你要部署在 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# 如果你要部署在 https://MySetsuna.github.io/hot-soup
-git push -f git@github.com:MySetsuna/hot-soup.git main:gh-pages
+# 如果你要部署在 https://MySetsuna.github.io
+git push -f git@github.com:MySetsuna/MySetsuna.github.io.git main:gh-pages
 
 cd -
